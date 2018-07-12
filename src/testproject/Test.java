eae2980c997ce27d47f69f548d6b9f9abdc5310f
shiba3ed <shiba3ed@CL263488.jvc.local>
2018-07-12 12:53:17 +0900

    tet

---
+++ src/testproject/Test.java
@@ -0,0 +1,21 @@
+package testproject;
+
+import java.util.ArrayList;
+
+public class Test {
+	public static void main(String[] args) {
+		ArrayList<String> array = new ArrayList<String>();
+		
+		array.add("test1");
+		array.add("test2");
+		array.add("test3");
+
+		array.add("test5");
+		array.add("test6");
+		
+		array.forEach(str ->
+		System.out.println(str));
+
+	}
+}
+

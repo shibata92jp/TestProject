3afd3527c4ee841935fd8234cbf21a8ebb5d4c1a
shiba3ed <shiba3ed@CL263488.jvc.local>
2018-07-12 10:22:41 +0900

    Test

--- src/testproject/Test.java
+++ src/testproject/Test.java
@@ -1,10 +1,20 @@
 package testproject;
 
-public class Test {
+import java.util.ArrayList;
 
+public class Test {
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+		ArrayList<String> array = new ArrayList<String>();
+		
+		array.add("test1");
+		array.add("test2");
+		array.add("test3");
+		array.add("test4");
+		System.out.println(array.get(1));
+		
+		array.forEach(str ->
+		System.out.println(str));
 
 	}
-
 }
+
